adced0afaf801f36eaf08d663826a987af3c5ad2
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2013-11-09 11:05:22 +0100

    yoeman generated app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('proxyMirrorApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
