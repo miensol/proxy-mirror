556dd2d8802fe3cad28b623b717548f8d58a8884
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2013-11-10 13:24:23 +0100

    basic headers display

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,23 @@
 (function(){
 
     var app = angular.module('proxyMirrorApp');
-    app.controller('MainCtrl', function ($scope, sessionStorage) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
 
+    var RequestViewModel = function(request) {
+        this.headers = request.uniformHeaders();
+    };
+
+    var ResponseViewModel = function(response){
+        this.headers = response.uniformHeaders();
+    };
+
+    app.controller('MainCtrl', function ($scope, sessionStorage) {
         $scope.sessions = sessionStorage.sessions;
+
+        $scope.selectSession = function(session){
+            sessionStorage.selectSession(session);
+            $scope.request = new RequestViewModel(session.request);
+            $scope.response = new ResponseViewModel(session.response);
+        };
     });
 
 
--- app/scripts/proxyClient.js
+++ app/scripts/proxyClient.js
@@ -1,57 +1,81 @@
-(function(){
+(function () {
 
     var proxyClient = angular.module('proxyMirrorApp.proxyClient', []);
 
-    var Session = function (id){
+    var Headers = function () {
+        var that = this;
+        this.uniformHeaders = function () {
+            return Object.keys(this.headers).map(function (key) {
+                var headerValue = that.headers[key];
+                var newName = key.split('-').map(function (oldName) {
+                    return oldName.substring(0, 1).toUpperCase() + oldName.substring(1);
+                }).join('-');
+                return [newName, headerValue];
+            }).reduce(function (acc, cur) {
+                    acc[cur[0]] = cur[1];
+                    return acc;
+                }, {});
+        };
+    };
+
+    var Session = function (id) {
         this.id = id;
         this.state = 'start';
         this.request = {};
         this.response = {};
-        this.started = function(sessionMsg){
+        this.started = function (sessionMsg) {
             this.request = sessionMsg.request;
+            Headers.call(this.request);
         };
-        this.ended = function(sessionMsg){
+        this.ended = function (sessionMsg) {
             this.response = sessionMsg.response;
             this.response.contentType = this.niceContentType();
+            Headers.call(this.response);
         };
 
-        this.niceContentType = function(){
+        this.niceContentType = function () {
             var fullContentType = this.response.headers['content-type'] || '';
             return fullContentType.split(';')[0] || '';
         };
     };
 
-    var SessionStorage = function (proxy){
+    var SessionStorage = function (proxy) {
         var that = this,
             sessionHash = {};
         this.proxy = proxy;
         this.sessions = [];
+        this.selectedSession = null;
 
-        proxy.on('session.start', function(sessionMsg){
+        proxy.on('session.start', function (sessionMsg) {
             var session = new Session(sessionMsg.id);
             sessionHash[session.id] = session;
             session.started(sessionMsg);
             that.sessions.push(session);
         });
-        proxy.on('session.end', function(sessionMsg){
+        proxy.on('session.end', function (sessionMsg) {
             var session = sessionHash[sessionMsg.id];
-            if(session){
+            if (session) {
                 session.ended(sessionMsg);
             }
         });
+
+        this.selectSession = function(session){
+            this.selectedSession = session;
+        };
+
     };
 
-    var ProxyConnection = function(socket){
+    var ProxyConnection = function (socket) {
         Observable(this);
         var that = this;
-        socket.on('connect', function(){
+        socket.on('connect', function () {
             that.state = 'connected';
         });
 
-        socket.on('session.start', function(session){
+        socket.on('session.start', function (session) {
             that.trigger('session.start', [session]);
         });
-        socket.on('session.end', function(session){
+        socket.on('session.end', function (session) {
             that.trigger('session.end', [session]);
         });
     };
@@ -81,12 +105,12 @@
     });
 
 
-    proxyClient.service('proxy', function(socket){
+    proxyClient.service('proxy', function (socket) {
         var proxyConnection = new ProxyConnection(socket);
         return proxyConnection;
     });
 
-    proxyClient.service('sessionStorage', function(proxy){
+    proxyClient.service('sessionStorage', function (proxy) {
         var sessionStorage = new SessionStorage(proxy);
 
         return  sessionStorage;
