34803b14f81c5efcd336567050b981c129b5ee2f
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2013-12-29 17:59:05 +0100

    basic support for https

--- setup-proxy.sh
+++ setup-proxy.sh
@@ -3,11 +3,14 @@
 function enableProxy {
   networksetup -setwebproxy 'Wi-Fi' localhost 8888 off
   networksetup -setwebproxystate 'Wi-Fi' on
+  networksetup -setsecurewebproxy 'Wi-Fi' localhost 8888 off
+  networksetup -setsecurewebproxystate 'Wi-Fi' on
   echo "Enabled proxy"
 }
 
 function disableProxy {
   networksetup -setwebproxystate 'Wi-Fi' off
+  networksetup -setsecurewebproxystate 'Wi-Fi' off
   echo "Disabled proxy"
 }
 
