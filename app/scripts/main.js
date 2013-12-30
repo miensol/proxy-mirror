f9fdc9f59aedfe3db715765e35f51b0884065e5f
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2013-12-30 12:43:20 +0100

    moving body as string and body as iframe to separate tabs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,90 @@
 (function(){
 
     var app = angular.module('proxyMirrorApp');
+    var noop = function(){};
 
-    var ResponseViewModel = function(response, $sce){
-        angular.extend(this, response);
-        var headers = this.headers || {};
-        this.displayAsIframe = !!this.bodyAsBase64;
-        this.iframeSrc = ['data:', headers['Content-Type'],';base64, ', this.bodyAsBase64].join('');
-        this.iframeSrc = $sce.trustAsResourceUrl(this.iframeSrc);
-    };
+    app.directive('pmInclude', ['$http', '$templateCache', '$anchorScroll', '$compile', '$animate', '$sce', '$parse',
+        function($http,   $templateCache,   $anchorScroll,   $compile,   $animate,   $sce, $parse) {
+            return {
+                restrict: 'ECA',
+                priority: 400,
+                terminal: true,
+                transclude: 'element',
+                compile: function(element, attr) {
+                    var srcExp = attr.pmInclude || attr.src,
+                        onloadExp = attr.onload || '',
+                        onchildloadExp = attr.childInit || '',
+                        autoScrollExp = attr.autoscroll;
+
+                    return function(scope, $element, $attr, ctrl, $transclude) {
+                        var changeCounter = 0,
+                            currentScope,
+                            currentElement;
+
+                        var cleanupLastIncludeContent = function() {
+                            if (currentScope) {
+                                currentScope.$destroy();
+                                currentScope = null;
+                            }
+                            if(currentElement) {
+                                $animate.leave(currentElement);
+                                currentElement = null;
+                            }
+                        };
+
+                        scope.$watch(srcExp, function pmIncludeWatchAction(src) {
+//                        scope.$watch($sce.parseAsResourceUrl(srcExp), function ngIncludeWatchAction(src) {
+                            var afterAnimation = function() {
+                                if (angular.isDefined(autoScrollExp) && (!autoScrollExp || scope.$eval(autoScrollExp))) {
+                                    $anchorScroll();
+                                }
+                            };
+                            var thisChangeId = ++changeCounter;
+
+                            if (src) {
+                                $http.get(src, {cache: $templateCache}).success(function(response) {
+                                    if (thisChangeId !== changeCounter) return;
+                                    var newScope = scope.$new();
+
+                                    // Note: This will also link all children of ng-include that were contained in the original
+                                    // html. If that content contains controllers, ... they could pollute/change the scope.
+                                    // However, using ng-include on an element with additional content does not make sense...
+                                    // Note: We can't remove them in the cloneAttchFn of $transclude as that
+                                    // function is called before linking the content, which would apply child
+                                    // directives to non existing elements.
+                                    var clone = $transclude(newScope, noop);
+                                    cleanupLastIncludeContent();
+
+                                    currentScope = newScope;
+                                    currentElement = clone;
+
+                                    currentElement.html(response);
+                                    $animate.enter(currentElement, null, $element, afterAnimation);
+                                    $compile(currentElement.contents())(currentScope);
+                                    currentScope.$emit('$includeContentLoaded');
+                                    scope.$eval(onloadExp);
+                                    var childInitFunc = currentScope.$eval(onchildloadExp);
+                                    if(typeof childInitFunc === 'function'){
+                                        childInitFunc(currentScope);
+                                    }
+                                }).error(function() {
+                                        if (thisChangeId === changeCounter) cleanupLastIncludeContent();
+                                    });
+                                scope.$emit('$includeContentRequested');
+                            } else {
+                                cleanupLastIncludeContent();
+                            }
+                        });
+                    };
+                }
+            };
+        }]
+    );
+
+
 
     app.controller('MainCtrl', function ($scope, $sce, sessionStorage) {
-        $scope.$watch(function(){
-            return sessionStorage.selectedSession;
-        }, function(session){
-            if(session){
-                $scope.request = session.request;
-                $scope.response = new ResponseViewModel(session.response, $sce);
-            }
-        });
+
     });
 
 
--- app/scripts/sessionDetails/sessionDetails.js
+++ app/scripts/sessionDetails/sessionDetails.js
@@ -20,11 +20,43 @@
         });
     };
 
+    var ResponseViewModel = function(response, $sce){
+        angular.extend(this, response);
+        var headers = this.headers || {};
+        this.displayAsIframe = !!this.bodyAsBase64;
+        this.iframeSrc = ['data:', headers['Content-Type'],';base64, ', this.bodyAsBase64].join('');
+        this.iframeSrc = $sce.trustAsResourceUrl(this.iframeSrc);
+    };
+
+    var SelectView = function(){
+        var $scope = this;
+        $scope.selectedView = null;
+        $scope.selectView = function(view){
+            $scope.selectedView = view;
+        };
+        $scope.classForView = function(view){
+            if(view === $scope.selectedView){
+                return 'active';
+            }
+            return '';
+        };
+    };
+
     module.controller('RequestDetailsCtrl', function($scope, sessionStorage){
         $scope.request = null;
+        var updateAvailableViews = function(){
+            $scope.availableViews = [{
+                title: 'Headers',
+                templateUrl: 'scripts/sessionDetails/requestHeaders.html'
+            }];
+            $scope.selectView($scope.availableViews[0]);
+        };
         watchForSelectedSession($scope, sessionStorage, function(session){
             $scope.request = session.request;
+            updateAvailableViews();
         });
+
+        SelectView.call($scope);
     });
 
     module.directive('pmResponseDetails', function(){
@@ -35,11 +67,38 @@
        };
     });
 
-    module.controller('ResponseDetailsCtrl', function($scope, sessionStorage){
+    module.controller('ResponseDetailsCtrl', function($scope,$sce, sessionStorage){
         $scope.response = null;
+        var updateAvailableViews = function(){
+            $scope.availableViews = [{
+                title: 'Headers',
+                templateUrl: 'scripts/sessionDetails/responseHeaders.html',
+                visible: true,
+                init: function($childScope){}
+            }, {
+                title: 'Body as Text',
+                templateUrl: 'scripts/sessionDetails/bodyAsString.html',
+                visible: !!$scope.response.bodyAsString,
+                init: function($childScope){
+                    $childScope.bodyAsString = $scope.response.bodyAsString;
+                }
+            }, {
+                title: 'Preview',
+                templateUrl: 'scripts/sessionDetails/bodyAsIframe.html',
+                visible: $scope.response.displayAsIframe,
+                init: function($childScope){
+                    $childScope.iframeSrc = $scope.response.iframeSrc;
+                }
+            }].filter(function(view){return view.visible;});
+            $scope.selectView($scope.availableViews[0]);
+        };
+
         watchForSelectedSession($scope, sessionStorage, function(session){
-            $scope.response = session.response;
+            $scope.response = new ResponseViewModel(session.response, $sce);
+            updateAvailableViews();
         });
+
+        SelectView.call($scope);
     });
 
 }());
